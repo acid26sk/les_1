a25f742de768a4fac665e62ba55916a2c62d52e4
Zhenya Zhdanov <57041103+acid26sk@users.noreply.github.com>
2020-07-14 18:43:17 +0300

    HW5

--- src/LESSON5/Empployee.java
+++ src/LESSON5/Empployee.java
@@ -1,7 +1,7 @@
 package LESSON5;
 
 class Employee {
-    String firstname, name, middlename, position, email;
+    String firstname, name, middlename, position, email,phone;
     int salary, age;
 
     Employee(String firstname,
@@ -9,6 +9,7 @@ class Employee {
              String middlename,
              String position,
              String email,
+             String phone,
              int salary,
              int age) {
         this.firstname = firstname;
@@ -16,14 +17,15 @@ class Employee {
         this.middlename = middlename;
         this.position = position;
         this.email = email;
+        this.phone=phone;
         this.salary = salary;
         this.age = age;
     }
 
     void getFullInfo() {
-        System.out.println("firstname: " + firstname + " | name: " + name
-                + " | middlename: " + middlename + " | position: " + position
-                + " | email: " + email + " | salary: " + salary + " | age: "
+        System.out.println("фамилия: " + firstname + " | имя: " + name
+                + " | отчество: " + middlename + " | должность: " + position
+                + " | email: " + email + " | телефон: " + phone +" | зарплата: " + salary + " | возраст: "
                 + age);
     }
 
@@ -35,16 +37,16 @@ class Employee {
 class HW5 {
     public static void main(String[] args) {
         Employee[] employees = new Employee[5];
-        employees[0] = new Employee("Ivanov", "Ivan", "Ivanovich",
-                "Top manager", "ivanovii@mail.me", 150000, 45);
-        employees[1] = new Employee("Petrov", "Petr", "Petrovich", "manager",
-                "petrovpp@mail.me", 50000, 30);
-        employees[2] = new Employee("Sidorov", "Sidor", "Sidorovich",
-                "manager", "sidorovss@mail.me", 35000, 25);
-        employees[3] = new Employee("Pupkin", "Vasilii", "Ivanovich",
-                "manager", "pupkinvi@mail.me", 650000, 41);
-        employees[4] = new Employee("Semenov", "Semen", "Semenovich",
-                "manager", "semenovss@mail.me", 300000, 23);
+        employees[0] = new Employee("Иванов", "Иван", "Иванович",
+                "Руководидель", "ivanov@mail.ru", "+7-928-551-41-41",150000, 46);
+        employees[1] = new Employee("Мачнев", "Александр", "Иванович",
+                "менеджер","machnev@mail.ru", "+7-988-458-62-54",50000, 32);
+        employees[2] = new Employee("Чернов", "Сергей", "Георгиевич",
+                "менеджер", "chernov@mail.ru", "+7-966-784-65-48", 35000, 27);
+        employees[3] = new Employee("Квашин", "Василий", "Дмитриевич",
+                "менеджер", "kvashin@mail.ru", "+7-962-231-12-45", 65000, 45);
+        employees[4] = new Employee("Полянский", "Олег", "Сергеевич",
+                "менеджер", "polyanski@mail.ru", "+7-919-458-78-98", 30000, 28);
 
         for (Employee e : employees) if (e.getAge() > 40) e.getFullInfo();
     }
