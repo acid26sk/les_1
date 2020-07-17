b5633954c5f99ee54591179a177d49bbd2773043
Zhenya Zhdanov <57041103+acid26sk@users.noreply.github.com>
2020-07-17 21:54:15 +0300

    HW6.1

--- src/LESSON6/HW6.java
+++ src/LESSON6/HW6.java
@@ -1,5 +1,7 @@
 package LESSON6;
 
+
+
 abstract class Animal {
     private final int MAX_RUN_LENGTH = 0;
     private final int MAX_SWIM_LENGTH = 0;
@@ -15,8 +17,11 @@ abstract class Animal {
 class Cat extends Animal {
     private final int MAX_RUN_LENGTH = 200;
     private final double MAX_JUMP_HEIGTH = 2;
+
+
     @Override
     void run(int length) {
+        System.out.println("Кот: \n");
         if ((length >= 0) && (length <= MAX_RUN_LENGTH)) System.out.println("БЕГ: true");
         else System.out.println("БЕГ: false");
     }
@@ -29,7 +34,7 @@ class Cat extends Animal {
     @Override
     void jump(double height) {
         if ((height >= 0) && (height <= MAX_JUMP_HEIGTH)) System.out.println("ПРЫЖКИ: true");
-        else System.out.println("ПРЫЖКИ: fase");
+        else System.out.println("ПРЫЖКИ: false");
     }
 }
 
@@ -40,6 +45,7 @@ class Dog extends Animal {
 
     @Override
     void run(int length) {
+        System.out.println("\nСобака: \n");
         if ((length >= 0) && (length <= MAX_RUN_LENGTH)) System.out.println("БЕГ: true");
     }
 
